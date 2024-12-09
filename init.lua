edb28a25833161195a2ab00bfc9b3cc06f67b276
Suno <ilovesuno@proton.me>
2024-12-08 19:19:28 -0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,79 +1,68 @@
--- Worth a try LOL
--- If this works, I am keeping it BECAUSE this is the only service I'll need to do this to
-
--- Literally every other service like this is useless
--- Don't bully me 😔
-
 return setmetatable({}, {
 	__index = function(self, service)
 		if service == "VirtualUser" then
-			return {
-				Button1Down = function(position, camera)
-					camera = camera or CFrame.identity
-						
-					return nil
-				end,
-					
-				Button1Up = function(position, camera)
-					camera = camera or CFrame.identity
-						
-					return nil
-				end,
-					
-				Button2Down = function(position, camera)
-					camera = camera or CFrame.identity
-						
-					return nil
-				end,
-					
-				Button2Up = function(position, camera)
-					camera = camera or CFrame.identity
-						
-					return nil
-				end,
-					
-				CaptureController = function()
-					return nil
-				end,
-					
-				ClickButton1 = function(position, camera)
-					camera = camera or CFrame.identity
-						
-					return nil
-				end,
-					
-				ClickButton2 = function(position, camera)
-					camera = camera or CFrame.identity
-						
-					return nil
-				end,
-					
-				MoveMouse = function(position, camera)
-					camera = camera or CFrame.identity
-						
-					return nil
-				end,
-					
-				SetKeyDown = function(key)
-					return nil
-				end,
-					
-				SetKeyUp = function(key)
-					return nil
-				end,
-					
-				StartRecording = function()
-					return nil
-				end,
-					
-				StopRecording = function()
-					return ""
-				end,
-					
-				TypeKey = function(key)
-					return nil
-				end
-			}
+			local virtualUser = {}
+			
+			function virtualUser:CaptureController()
+				return nil
+			end
+			
+			function virtualUser:Button1Down(position, camera)
+				camera = camera or CFrame.identity
+				return nil
+			end
+			
+			function virtualUser:Button1Up(position, camera)
+				camera = camera or CFrame.identity
+				return nil
+			end
+			
+			function virtualUser:Button2Down(position, camera)
+				camera = camera or CFrame.identity
+				return nil
+			end
+			
+			function virtualUser:Button2Up(position, camera)
+				camera = camera or CFrame.identity
+				return nil
+			end
+			
+			function virtualUser:ClickButton1(position, camera)
+				camera = camera or CFrame.identity
+				return nil
+			end
+			
+			function virtualUser:ClickButton2(position, camera)
+				camera = camera or CFrame.identity
+				return nil
+			end
+			
+			function virtualUser:MoveMouse(position, camera)
+				camera = camera or CFrame.identity
+				return nil
+			end
+			
+			function virtualUser:SetKeyDown(key)
+				return nil
+			end
+			
+			function virtualUser:SetKeyUp(key)
+				return nil
+			end
+			
+			function virtualUser:StartRecording()
+				return nil
+			end
+			
+			function virtualUser:StopRecording()
+				return ""
+			end
+			
+			function virtualUser:TypeKey(key)
+				return nil
+			end
+			
+			return virtualUser
 		end
 		
 		local __service = game:GetService(service) do
